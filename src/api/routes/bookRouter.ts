cc3146a4745eb04e1cd86602927d4a6d228150fe
AhmeddOsama <ahmeddosama@hotmail.com>
2023-12-24 22:25:24 +0200

    renamed delete path and update path

--- src/api/routes/bookRouter.ts
+++ src/api/routes/bookRouter.ts
@@ -13,8 +13,8 @@ const validateBookQueryParams = validateQueryParams({
 });
 
 router.post('/add', rateLimitMiddleware, bookController.createBook.bind(bookController));
-router.put('/:isbn', bookController.updateBook.bind(bookController));
-router.delete('/:isbn', bookController.deleteBook.bind(bookController));
+router.put('update/:isbn', bookController.updateBook.bind(bookController));
+router.delete('delete/:isbn', bookController.deleteBook.bind(bookController));
 //Validate Query Params is used in routes where the query parameters are defined in the url
 router.get('', rateLimitMiddleware, validateBookQueryParams, bookController.searchBook.bind(bookController));
 
