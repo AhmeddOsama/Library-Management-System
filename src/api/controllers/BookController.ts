d1321fd2a5b8f6a0ed1e5aae39e8cb71d19ed5ff
AhmeddOsama <ahmeddosama@hotmail.com>
2023-12-24 17:05:48 +0200

    added request body validations for the book controller

--- src/api/controllers/BookController.ts
+++ src/api/controllers/BookController.ts
@@ -1,13 +1,29 @@
 import { Request, Response } from 'express';
 import { BookService } from '../../services/BookService';
+import { validateBody } from '../../decorators/validateRequestBody'
+import Joi from 'joi';
 
+const createBookBody = Joi.object({
+    title: Joi.string().required(),
+    author: Joi.string().required(),
+    isbn: Joi.string().required(),
+    quantity: Joi.number().required(),
+
+})
+const bookBody = Joi.object({
+    title: Joi.string().optional(),
+    author: Joi.string().optional(),
+    isbn: Joi.string().required(),
+    quantity: Joi.number().optional(),
+
+})
 export class BookController {
     private readonly bookService: BookService;
 
     constructor() {
         this.bookService = new BookService();
     }
-
+    @validateBody(createBookBody)
     async createBook(req: Request, res: Response) {
         try {
             const book = await this.bookService.createBook(req.body);
@@ -17,7 +33,7 @@ export class BookController {
         }
     }
 
-
+    @validateBody(bookBody)
     async updateBook(req: Request, res: Response) {
         try {
             const book = await this.bookService.updateBook(req.params.isbn, req.body);
